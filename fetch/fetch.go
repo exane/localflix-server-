26ba2e83777638975e59e3d5441f7a50502d46ba
Tim Meier <raco0n@gmx.de>
2016-06-11 13:21:16 +0200

    add fetch test

--- fetch/fetch.go
+++ fetch/fetch.go
@@ -1,123 +1,164 @@
 package fetch
 
 import (
-  "io/ioutil"
-  "encoding/json"
-  "os"
-  "regexp"
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"regexp"
 )
 
 const (
-  PATH = "Z:/serien"
-  OUT_PATH = "Y:/golangWorkspace/src/github.com/exane/localflix-server-/fetch"
-  OUT = "DATA_DUMP"
-  VIDEO = ".*[.](avi|web|mkv|mp4)$"
-  SUBTITLE = ".*[.](srt|idx|sub|sfv)$"
+	PATH     = "Z:/serien"
+	OUT_PATH = "Y:/golangWorkspace/src/github.com/exane/localflix-server-/fetch"
+	OUT      = "DATA_DUMP"
+	VIDEO    = ".*[.](avi|web|mkv|mp4)$"
+	SUBTITLE = ".*[.](srt|idx|sub|sfv)$"
+	IGNORE   = ".*[.](json)$"
 )
 
-func Fetch() {
-  series := []Serie{}
+type FileType interface {
+	ReadDir(path string) ([]os.FileInfo, error)
+	WriteJSON(filename string, data []byte) error
+	PathRead() string
+	PathWrite() string
+}
 
-  files, _ := ioutil.ReadDir(PATH)
-  for i, val := range files {
-    println(val.Name())
-    series = append(series, Serie{Name: val.Name()})
-    series[i].fetchSeasons(PATH)
-  }
-  js, _ := json.Marshal(series)
-  ioutil.WriteFile(OUT_PATH + "/" + OUT + ".json", []byte(js), 0666)
+type File struct{}
+
+func (File) ReadDir(path string) ([]os.FileInfo, error) {
+	return ioutil.ReadDir(path)
+}
+func (File) WriteJSON(filename string, data []byte) error {
+	return ioutil.WriteFile(filename, data, 0666)
+}
+func (File) PathRead() string {
+	return PATH
+}
+func (File) PathWrite() string {
+	return OUT_PATH
+}
+
+func Fetch() {
+	fetch(File{})
+}
+
+func fetch(file FileType) []byte {
+	series := []Serie{}
+
+	path := file.PathRead()
+
+	files, _ := file.ReadDir(path)
+
+	for _, val := range files {
+		if ignore(val.Name()) {
+			continue
+		}
+		serie := Serie{Name: val.Name()}
+		serie.fetchSeasons(path, file)
+		series = append(series, serie)
+	}
+
+	js, _ := json.Marshal(series)
+	file.WriteJSON(file.PathWrite()+"/"+OUT+".json", []byte(js))
+	return js
 }
 
 type Serie struct {
-  Name    string
-  Seasons []*Season
+	Name    string
+	Seasons []*Season
 }
 
 type Season struct {
-  Name     string
-  SerieName string
-  Episodes []*Episode
+	Name      string
+	SerieName string
+	Episodes  []*Episode
 }
 
 type Episode struct {
-  Name      string
-  Extension string
-  Subtitles []Subtitle
-  Src       string
+	Name      string
+	Extension string
+	Subtitles []Subtitle
+	Src       string
 }
 
 type Subtitle struct {
-  Name string
+	Name string
 }
 
 func (season *Season) findEpisode(name string) (*Episode, bool) {
-  for _, val := range season.Episodes {
-    if val.Name == name {
-      return val, true
-    }
-  }
-  return &Episode{}, false
+	for _, val := range season.Episodes {
+		if val.Name == name {
+			return val, true
+		}
+	}
+	return &Episode{}, false
 }
 
-func (serie *Serie) fetchSeasons(path string) {
-  newPath := path + "/" + serie.Name
-  files, _ := ioutil.ReadDir(newPath)
+func (serie *Serie) fetchSeasons(path string, file FileType) {
+	newPath := path + "/" + serie.Name
+	files, _ := file.ReadDir(newPath)
 
-  for i, val := range files {
-    serie.Seasons = append(serie.Seasons, &Season{Name: val.Name(), SerieName: serie.Name})
-    serie.Seasons[i].fetchEpisodes(newPath)
-  }
+	for i, val := range files {
+		serie.Seasons = append(serie.Seasons, &Season{Name: val.Name(), SerieName: serie.Name})
+		serie.Seasons[i].fetchEpisodes(newPath, file)
+	}
 }
 
-func (season *Season) fetchEpisodes(path string) {
-  newPath := path + "/" + season.Name
-  files, _ := ioutil.ReadDir(newPath)
+func (season *Season) fetchEpisodes(path string, file FileType) {
+	newPath := path + "/" + season.Name
+	files, _ := file.ReadDir(newPath)
 
-  for _, val := range files {
-    episode := &Episode{
-      Name: filename(val.Name()),
-      Extension: "",
-      Subtitles: []Subtitle{},
-    }
+	for _, val := range files {
+		episode := &Episode{
+			Name:      filename(val.Name()),
+			Extension: "",
+			Subtitles: []Subtitle{},
+		}
 
-    if isValid(VIDEO, val) {
-      ep, exist := season.findEpisode(filename(val.Name()))
-      if !exist {
-        season.Episodes = append(season.Episodes, episode)
-        ep = episode
-      }
-      if len(ep.Extension) == 0 {
-        ep.Extension = extension(val.Name())
-      }
-      ep.Src = newPath
-    }
+		if isValid(VIDEO, val) {
+			ep, exist := season.findEpisode(filename(val.Name()))
+			if !exist {
+				season.Episodes = append(season.Episodes, episode)
+				ep = episode
+			}
+			if len(ep.Extension) == 0 {
+				ep.Extension = extension(val.Name())
+			}
+			ep.Src = newPath
+		}
 
-    if isValid(SUBTITLE, val) {
-      name := filename(val.Name())
+		if isValid(SUBTITLE, val) {
+			name := filename(val.Name())
 
-      ep, exist := season.findEpisode(name)
-      if !exist {
-        season.Episodes = append(season.Episodes, episode)
-        ep = episode
-      }
-      ep.Subtitles = append(ep.Subtitles, Subtitle{Name: val.Name()})
-    }
-  }
+			ep, exist := season.findEpisode(name)
+			if !exist {
+				season.Episodes = append(season.Episodes, episode)
+				ep = episode
+			}
+			ep.Subtitles = append(ep.Subtitles, Subtitle{Name: val.Name()})
+		}
+	}
 }
 
 func isValid(TYPE string, file os.FileInfo) bool {
-  matched, _ := regexp.MatchString(TYPE, file.Name())
-  return matched
+	matched, _ := regexp.MatchString(TYPE, file.Name())
+	return matched
 }
 
 func filename(name string) string {
-  regex := regexp.MustCompile("^(.*)[.].*")
-
-  return regex.ReplaceAllString(name, "$1")
+	regex := regexp.MustCompile("^(.*)[.].*")
+	return regex.ReplaceAllString(name, "$1")
 }
 
 func extension(name string) string {
-  regex := regexp.MustCompile(".*[.](.*)$")
+	regex := regexp.MustCompile(".*[.](.*)$")
+	return regex.ReplaceAllString(name, "$1")
+}
 
-  return regex.ReplaceAllString(name, "$1")
+func ignore(name string) bool {
+	ok, err := regexp.Match(IGNORE, []byte(name))
+	if err != nil {
+		println(err)
+	}
+	return ok
 }
---
+++ fetch/fetch_test.go
@@ -0,0 +1,102 @@
+package fetch
+
+import (
+  . "github.com/onsi/ginkgo"
+  . "github.com/onsi/gomega"
+  "os"
+  "io/ioutil"
+  "encoding/json"
+)
+
+type testFile struct{}
+
+func (testFile) ReadDir(path string) ([]os.FileInfo, error) {
+  return ioutil.ReadDir(path)
+}
+
+func (testFile) WriteJSON(filename string, data []byte) error {
+  return nil
+}
+
+func (testFile) PathRead() string {
+  return "./test_data"
+}
+
+func (testFile) PathWrite() string {
+  return "./test_data"
+}
+
+var _ = Describe("Fetch", func() {
+  Describe("Fetch", func() {
+    var series = []Serie{}
+
+    BeforeEach(func() {
+      js := fetch(testFile{})
+      series = []Serie{}
+      json.Unmarshal(js, &series)
+    })
+
+    Describe("serie", func() {
+      It("should not be nil", func() {
+        Expect(series).To(BeAssignableToTypeOf([]Serie{}))
+        Expect(series[0]).ToNot(BeNil())
+        Expect(series[1]).ToNot(BeNil())
+      })
+
+      It("should have 2 series", func() {
+        Expect(len(series)).To(Equal(2))
+        Expect(series[0]).To(BeAssignableToTypeOf(Serie{}))
+        Expect(series[1]).To(BeAssignableToTypeOf(Serie{}))
+      })
+    })
+
+    Describe("season", func() {
+      It("should have 4 seasons in total", func() {
+        for _, serie := range series {
+          Expect(serie.Seasons).To(BeAssignableToTypeOf([]*Season{}))
+          Expect(len(serie.Seasons)).To(Equal(2))
+        }
+      })
+    })
+
+    Describe("episode", func() {
+      It("each season should have 1 episode", func() {
+        for _, serie := range series {
+          for _, season := range serie.Seasons {
+            Expect(season.Episodes).To(BeAssignableToTypeOf([]*Episode{}))
+            Expect(len(season.Episodes)).To(Equal(1))
+          }
+        }
+      })
+    })
+
+    Describe("subtitles", func() {
+      It("each episode should have 1 subtitle", func() {
+        for _, serie := range series {
+          for _, season := range serie.Seasons {
+            for _, episode := range season.Episodes {
+              Expect(episode.Subtitles).To(BeAssignableToTypeOf([]Subtitle{}))
+              Expect(len(episode.Subtitles)).To(Equal(1))
+            }
+          }
+        }
+      })
+    })
+  })
+
+  Describe("filename", func() {
+    It("should work as intended", func() {
+      Expect(filename("01.avi")).To(Equal("01"))
+      Expect(filename(".01.avi")).To(Equal(".01"))
+      Expect(filename(".01.avi.")).To(Equal(".01.avi"))
+      Expect(filename("01.avi.")).To(Equal("01.avi"))
+      Expect(filename(".avi.")).To(Equal(".avi"))
+      Expect(filename(".avi..")).To(Equal(".avi."))
+      Expect(filename("..avi..")).To(Equal("..avi."))
+    })
+    It("should return everything before the last dot", func() {
+      name := "Game.of.Thrones.S06E07.HDTV.x264-KILLERS.mkv"
+      Expect(filename(name)).To(Equal("Game.of.Thrones.S06E07.HDTV.x264-KILLERS"))
+    })
+  })
+})
